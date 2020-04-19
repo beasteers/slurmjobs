3c3cab2718bc1e9b31baad8259415448395b112c
Bea Steers <bea.steers@gmail.com>
2020-04-19 12:15:23 -0400

    fixing install error

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
 setuptools.setup(name='slurmjobs',
-                 version='0.0.2',
+                 version='0.0.3',
                  description='Generate slurm jobs in batches.',
                  long_description=open('README.md').read().strip(),
                  long_description_content_type='text/markdown',
                  author='Bea Steers',
                  author_email='bea.steers@gmail.com',
                  # url='http://path-to-my-packagename',
-                 package_data={'slurmjobs': {'templates/*.j2'}},
+                 package_data={'slurmjobs': ['templates/*.j2']},
                  packages=setuptools.find_packages(),
                  install_requires=['pathtree', 'Jinja2'],
                  license='MIT License',
